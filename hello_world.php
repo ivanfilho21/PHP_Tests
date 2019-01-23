f94fe5d7fff44a50a68b9ac3c61ad92cdecb7f25
Ivan Costa do Nascimento Filho <ivanfilho21@gmail.com>
2019-01-22 23:31:01 -0200

    My first PHP code with some comments.

--- hello_world.php
+++ hello_world.php
@@ -1,4 +1,5 @@
 <?php
+date_default_timezone_set ("America/Bahia");
 
 echo "Hello World. Today is " . date('d/m/Y');
 echo " and it's " . date('H:i:s');
@@ -10,6 +11,6 @@ echo "      . Test: " . date("H:i:s a") . " " . date("M");
 # m means month number. M means month name abbreviated.
 # H means 24 hours format.
 # i means minutes.
-# Apparently it is OK to use double quotes.
+# Apparently it is OK to use double quotes in 'echo'.
 # a will display am or pm.
 # A will display AM or PM.
