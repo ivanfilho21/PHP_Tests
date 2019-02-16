b4783ccbf5681ff6f2e8af3c1de674dbdfd6f930
ivanfilho21 <ivanfilho21@gmail.com>
2019-02-16 10:46:17 -0300

    Changed index layout. Hide table contents (to be moved to another page later).

--- crud_mysql/scripts/index/user_logged.php
+++ crud_mysql/scripts/index/user_logged.php
@@ -1,36 +1,56 @@
 
+<h1>Tables</h1>
+
 <input type="submit" value="Create Table" onClick="parent.location='create-table.php'">
 
-<?php foreach ($tables as $value) : ?>
-	<?php foreach ($value as $name) : ?>
+<table>
+	
+	<thead>
+		<th>Name</th>
+		<th>Action</th>
+	</thead>
 
-		<h4><?php echo $name; ?></h4>
+	<tbody>
+		<?php foreach ($tables as $value) : ?>
+			<?php foreach ($value as $name) : ?>
+				
+				<tr>
+					<td>
 
-		<div class="table-holder">
-			<input type="submit" value="-" onclick="" id="delete-row">
-			<table>
-				<?php $columns = getTableColumns($connection, $name); ?>
+						<a href=""><?php echo $name; ?></a>
 
-				<thead>
-					<?php foreach ($columns as $column) : ?>
-						<?php foreach ($column as $cName) : ?>
-							<th><?php echo $cName; ?></th>
-						<?php endforeach; ?>
-					<?php endforeach; ?>
-				</thead>
+						<table style="display: none;">
+							<?php $columns = getTableColumns($connection, $name); ?>
 
-				<tbody>
-					<?php $rows = getTableContent($connection, $name); ?>
-					<tr>
-						<?php foreach ($rows as $row) : ?>
-							<?php foreach ($row as $rName) : ?>
-								<td><?php echo $rName; ?></td>
-							<?php endforeach; ?>
-						<?php endforeach; ?>
-					</tr>
-				</tbody>
-			</table>
-		</div>
+							<thead>
+								<?php foreach ($columns as $column) : ?>
+									<?php foreach ($column as $cName) : ?>
+										<th><?php echo $cName; ?></th>
+									<?php endforeach; ?>
+								<?php endforeach; ?>
+							</thead>
 
-	<?php endforeach; ?>
-<?php endforeach; ?>
+							<tbody>
+								<?php $rows = getTableContent($connection, $name); ?>
+								<tr>
+									<?php foreach ($rows as $row) : ?>
+										<?php foreach ($row as $rName) : ?>
+											<td><?php echo $rName; ?></td>
+										<?php endforeach; ?>
+									<?php endforeach; ?>
+								</tr>
+							</tbody>
+						</table>
+					</td>
+
+					<td>
+						<!--
+						<input type="submit" value="Update" onclick="" id="update-table">
+						<div style="display: inline-block; height: 16px; border-left: 1px solid #a09d9d; vertical-align: middle;"></div>-->
+						<input type="submit" value="Delete" onclick="" id="delete-table">
+					</td>
+				</tr>
+			<?php endforeach; ?>
+		<?php endforeach; ?>
+	</tbody>
+</table>
