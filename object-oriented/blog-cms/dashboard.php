cbb63f571f72dd31c4480bfbf7f057841d74e596
ivanfilho21 <ivanfilho21@gmail.com>
2019-03-12 18:09:51 -0300

    Added meta tags to dashboard

--- object-oriented/blog-cms/dashboard.php
+++ object-oriented/blog-cms/dashboard.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="utf-8">
 	<title>Dashboard</title>
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
 	<h1>Welcome to your Dashboard</h1>
