89915a5794ba73f19b710827638196e07c52e975
ivanfilho21 <ivanfilho21@gmail.com>
2019-03-23 15:01:46 -0300

    Second commit

--- object-oriented/agenda/Contact.php
+++ object-oriented/agenda/Contact.php
@@ -7,63 +7,95 @@ define("DB_PASS", "");
 
 class Contact
 {
-	private $db;
+    private $db;
 
-	public function __construct()
-	{
-		$dsn = DB_TYPE . ":dbname=" . DB_NAME . ";host=" . DB_HOST;
+    public function __construct()
+    {
+        $dsn = DB_TYPE . ":dbname=" . DB_NAME . ";host=" . DB_HOST;
         $dbUser = DB_USER;
         $dbPass = DB_PASS;
 
-		$this->db = new PDO($dsn, $dbUser, $dbPass);
-		# echo "Connected to Database.";
-	}
+        $this->db = new PDO($dsn, $dbUser, $dbPass);
+        # echo "Connected to Database.";
+    }
 
-	public function add($email, $name = "")
-	{
-		# check if email exists
-		# add only if false
+    public function add($email, $name = "")
+    {
+        # check if email exists
+        # add only if false
 
-		if (! $this->emailExists()) {
-			$sql = "INSERT INTO contacts (name, email) VALUES (:name, :email)";
-			$sql = $this->db->prepare($sql);
-			$sql->bindValue(":name", $name);
-			$sql->bindValue(":email", $email);
-			$sql->execute();
+        if (! $this->emailExists($email)) {
+            $sql = "INSERT INTO contacts (name, email) VALUES (:name, :email)";
+            $sql = $this->db->prepare($sql);
+            $sql->bindValue(":name", $name);
+            $sql->bindValue(":email", $email);
+            $sql->execute();
 
-			return true;
-		}
-		return false;
-	}
+            return true;
+        }
+        return false;
+    }
 
-	public function getName($email)
-	{
-		$sql = "SELECT name FROM contacts WHERE email = :email";
-		$sql = $this->db->prepare($sql);
-		$sql->bind(":email", $email);
-		$res = $sql->execute();
+    public function getName($email)
+    {
+        $sql = "SELECT name FROM contacts WHERE email = :email";
+        $sql = $this->db->prepare($sql);
+        $sql->bindValue(":email", $email);
+        $sql->execute();
 
-		if ($res->rowCount() > 0) {
-			$info = $res->fetch();
+        if ($sql->rowCount() > 0) {
+            $info = $sql->fetch();
 
-			return $info["name"];
-		}
-		return "";
-	}
+            return $info["name"];
+        }
+        return "";
+    }
 
-	public function getAll()
-	{
-		$sql = "SELECT * FROM contacts";
-		$res = $this->db->query($sql);
+    public function getAll()
+    {
+        $sql = "SELECT * FROM contacts";
+        $res = $this->db->query($sql);
 
-		if ($res->rowCount() > 0) {
-			return $res->fetchAll();
-		}
-		return array();
-	}
+        if ($res->rowCount() > 0) {
+            return $res->fetchAll();
+        }
+        return array();
+    }
 
-	private function emailExists()
-	{
-		
-	}
+    public function update($newName,$email)
+    {
+        if ($this->emailExists($email)) {
+            $sql = "UPDATE contacts SET name = :name WHERE email = :email";
+            $sql = $this->db->dd->prepare($sql);
+            $sql->bindValue(":name", $name);
+            $sql->bindValue(":email", $email);
+            $sql->execute();
+
+            return true;
+        }
+        return false;
+    }
+
+    public function delete($email)
+    {
+        if ($this->emailExists($email)) {
+            $sql = "DELETE FROM contacts WHERE email = :email";
+            $sql = $this->db->prepare($sql);
+            $sql->bindValue(":email", $email);
+            $sql->execute();
+
+            return true;
+        }
+        return false;
+    }
+
+    private function emailExists($email)
+    {
+        $sql = "SELECT * FROM contacts WHERE email = :email";
+        $sql = $this->db->prepare($sql);
+        $sql->bindValue(":email", $email);
+        $sql->execute();
+
+        return ($sql->rowCount() > 0);
+    }
 }
--- object-oriented/agenda/index.php
+++ object-oriented/agenda/index.php
@@ -1,10 +1,39 @@
-<?php require "Contact.php"; new Contact(); ?>
+<?php require "Contact.php"; $contacts = new Contact(); ?>
 <!DOCTYPE html>
 <html>
 <head>
     <title>My Agenda - Home Page</title>
 </head>
 <body>
-    
+	<h1>My Agenda - Contacts</h1>
+	<?php if (count($contacts->getAll()) > 0) : ?>
+    	<table border="1">
+    		<tr>
+    			<th>ID</th>
+    			<th>E-mail</th>
+    			<th>Name</th>
+    			<th>Action</th>
+    		</tr>
+
+    		<?php foreach($contacts->getAll() as $contact) : ?>
+    			<tr>
+    				<td>
+    					<?php echo $contact["id"]; ?>
+    				</td>
+    				<td>
+    					<?php echo $contact["email"]; ?>
+    				</td>
+    				<td>
+    					<?php echo $contact["name"]; ?>
+    				</td>
+
+    				<td>
+    					<a href="update.php?email=<?php echo $contact["email"]; ?>">Edit</a>
+    					<a href="delete.php?email=<?php echo $contact["email"]; ?>">Delete</a>
+    				</td>    			
+    			</tr>
+    		<?php endforeach; ?>
+    	</table>
+	<?php endif; ?>
 </body>
 </html>
