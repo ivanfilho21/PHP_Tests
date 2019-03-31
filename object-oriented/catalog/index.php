203087f798565a9202d0dbdd987aafa72952e12b
ivanfilho21 <ivanfilho21@gmail.com>
2019-03-31 11:25:17 -0300

    Updating

--- object-oriented/catalog/index.php
+++ object-oriented/catalog/index.php
@@ -1,55 +1,78 @@
 <?php require "pages/header.php"; ?>
 <?php require "index-script.php"; ?>
-	<section class="card">
+	<section class="card" style="display: none;">
 		<h1>We now have <?php echo $totalAnnouncements; ?>  advertisement<?php echo ($totalAnnouncements > 1) ? "s" : ""; ?></h1>
 		<h3>And <?php echo $totalUsers; ?> registered user<?php echo ($totalUsers > 1) ? "s" : ""; ?></h3>
 	</section>
 
-	<section class="card">
-		<h3>Advanced Search</h3>
+	<div class="index-wrapper">
+		<section class="advanced-search">
+			<h3>Advanced Search</h3>
 
-		<form method="GET">
-			<label>By Category</label>
-			<ul class="ul">
-				<?php foreach ($categories->getAll() as $category) : ?>
-				<li>
-					<input type="checkbox" name="filter[category][]" id="category<?php echo $category['id']; ?>"> <label class="checkbox-label" for="category<?php echo $category['id']; ?>"><?php echo $category['name']; ?></label>
-				</li>
-				<?php endforeach; ?>
-			</ul>
+			<form method="GET" action="index-advanced-search.php">
+				<label>By Category</label>
+				<ul class="ul">
+					<?php foreach ($categories->getAll() as $category) : ?>
+					<li>
+						<input type="checkbox" name="filter[category][]" id="category<?php echo $category['id']; ?>"> <label class="label-option" for="category<?php echo $category['id']; ?>"><?php echo $category['name']; ?></label>
+					</li>
+					<?php endforeach; ?>
+				</ul>
+
+				<label>By Price</label>
+				<ul class="ul">
+					<li>
+						<input type="radio" name="filter[price-range]" id="range-min"> <label for="range-min" class="label-option">Minimum</label>
+					</li>
+					<li>
+						<input type="radio" name="filter[price-range]" id="range-max"> <label for="range-max" class="label-option">Maximum</label>
+					</li>
+				</ul>
+				<input type="text" name="filter[price]" placeholder="Price">
+
+				<label>By Condition</label>
+				<ul class="ul">
+					<li>
+						<input type="radio" name="filter[condition]" id="condition-new"> <label for="condition-new" class="label-option">New</label>
+					</li>
+					<li>
+						<input type="radio" name="filter[condition]" id="condition-used"> <label for="condition-used" class="label-option">Used</label>
+					</li>
+				</ul>
+
+				<input type="submit" name="search" value="Search" class="btn btn-default">
+			</form>
+		</section>
+
+		<section class="newest-announcements">
+			<h3>Newest Announcements</h3>
+			<table class="table table-stripped">
+				<tbody>
+					<?php foreach ($announcements->getLatest($database, $currentPage, $maxPerPage) as $a) : ?>
+					<tr>
+						<td>
+							<img class="thumb" src="<?php echo ANNOUNCEMENT_PICTURES_DIR ."/"; echo (isset($a['url'])) ? $a['url'] : 'default.svg'; ?>" alt="Announcement Picture" border="0">
+						</td>
+
+						<td>
+							<p><a href="announcement-view.php?id=<?php echo $a['id']; ?>"><?php echo $a["title"]; ?></a></p>
+							<p><?php echo $a["categoryName"]; ?></p>
+						</td>
+
+						<td>
+							<p>US$ <?php echo $a["price"]; ?></p>
+						</td>
+					</tr>
+					<?php endforeach; ?>
+				</tbody>
+			</table>
+			<div class="pagination">
+				<?php for ($i = 1; $i <= $maxPages; $i++) : ?>
+				<a <?php echo ($i == $currentPage) ? 'class="active"' : ''; ?> href="index.php?p=<?php echo ($i); ?>"><?php echo ($i); ?></a>
+				<?php endfor; ?>
+			</div>
 			
-			<input type="submit" name="search" value="Search" class="btn btn-default">
-		</form>
-	</section>
-
-	<section class="card">
-		<h3>Newest Announcements</h3>
-		<table class="table table-stripped">
-			<tbody>
-				<?php foreach ($announcements->getLatest($database, $currentPage, $maxPerPage) as $a) : ?>
-				<tr>
-					<td>
-						<img class="thumb" src="<?php echo ANNOUNCEMENT_PICTURES_DIR ."/"; echo (isset($a['url'])) ? $a['url'] : 'default.svg'; ?>" alt="Announcement Picture" border="0">
-					</td>
-
-					<td>
-						<p><a href="announcement-view.php?id=<?php echo $a['id']; ?>"><?php echo $a["title"]; ?></a></p>
-						<p><?php echo $a["categoryName"]; ?></p>
-					</td>
-
-					<td>
-						<p>US$ <?php echo $a["price"]; ?></p>
-					</td>
-				</tr>
-				<?php endforeach; ?>
-			</tbody>
-		</table>
-		<div class="pagination">
-			<?php for ($i = 1; $i <= $maxPages; $i++) : ?>
-			<a <?php echo ($i == $currentPage) ? 'class="active"' : ''; ?> href="index.php?p=<?php echo ($i); ?>"><?php echo ($i); ?></a>
-			<?php endfor; ?>
-		</div>
-		
-	</section>
+		</section>
+	</div>
 
 <?php require "pages/footer.php"; ?>
--- object-oriented/catalog/pages/header.php
+++ object-oriented/catalog/pages/header.php
@@ -10,6 +10,7 @@
 	<link rel="stylesheet" href="assets/css/general.css">
 	<link rel="stylesheet" href="assets/css/header.css">
 	<link rel="stylesheet" href="assets/css/footer.css">
+	<link rel="stylesheet" href="assets/css/index.css">
 	<!-- Mobile specific -->
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="theme-color" content="darkslategray" />
@@ -19,6 +20,11 @@
 		<div class="nav-bar-header">
 			<a href="index.php">Catalog</a>
 		</div>
+		<form class="search-bar" method="GET">
+			<input type="text" name="search-text" placeholder="Search">
+			<input type="submit" name="search" value="?" class="btn btn-search">
+			<a href="#" class="btn-advanced-search btn btn-default">#</a>
+		</form>
 		<ul class="nav-bar-menu-list">
 			<?php if (! empty(getUserSession())) : ?>
 				<li><a href="my-announcements.php">My Announcements</a></li>
@@ -28,4 +34,5 @@
 				<li><a href="login.php">Login</a></li>
 			<?php endif; ?>
 		</ul>
+		<div class="clear-fix"></div>
 	</nav>
