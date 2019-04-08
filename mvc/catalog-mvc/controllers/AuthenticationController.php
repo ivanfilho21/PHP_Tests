2877ed1b19729d0810ec25e29cdad6b48c291b41
ivanfilho21 <ivanfilho21@gmail.com>
2019-04-08 12:46:45 -0300

    Added authentication controller and views

---
+++ mvc/catalog-mvc/controllers/AuthenticationController.php
@@ -0,0 +1,18 @@
+<?php
+class AuthenticationController extends Controller
+{
+	public function index()
+	{
+		#
+	}
+
+	public function login()
+	{
+		#
+	}
+
+	public function register()
+	{
+		#
+	}
+}
