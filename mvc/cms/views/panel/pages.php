12fe38f4cb082077baafe6e3cc6e9cf8700cfa23
ivanfilho21 <ivanfilho21@gmail.com>
2019-04-17 14:40:14 -0300

    Updating

--- mvc/cms/views/panel/pages.php
+++ mvc/cms/views/panel/pages.php
@@ -3,6 +3,7 @@
 	<h1>Your Pages</h1>
 
 	<div class="preview-pages">
+		<a class="page-preview" href="<?php echo BASE_URL; ?>panel/create/pages" style="display: flex; flex-direction: column; align-items: center; justify-content: center;"><i class="fas fa-plus" style="font-size: 1.5em;"></i></a>
 	<?php foreach ($pages as $p) : ?>
 		<a class="page-preview" href="<?php echo BASE_URL .$p['url']; ?>" target="_blank">
 			<div>
