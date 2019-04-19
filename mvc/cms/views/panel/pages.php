2a6148ca2877cdd5c70f2aa0100a1cd70bfa46c2
ivanfilho21 <ivanfilho21@gmail.com>
2019-04-19 10:15:36 -0300

    Updating

--- mvc/cms/views/panel/pages.php
+++ mvc/cms/views/panel/pages.php
@@ -3,16 +3,25 @@
 	<h1>Your Pages</h1>
 
 	<div class="preview-pages">
-		<a class="page-preview" href="<?php echo BASE_URL; ?>panel/create/pages" style="display: flex; flex-direction: column; align-items: center; justify-content: center;"><i class="fas fa-plus" style="font-size: 1.5em;"></i></a>
+		<a class="page-preview" href="<?php echo BASE_URL; ?>panel/create/pages" style="display: flex; flex-direction: column; align-items: center; justify-content: center; padding: 1em;"><i class="fas fa-plus" style="font-size: 1.5em;"></i></a>
+	<?php if ($pages !== false) : ?>
 	<?php foreach ($pages as $p) : ?>
-		<a class="page-preview" href="<?php echo BASE_URL .$p['url']; ?>" target="_blank">
-			<div>
-				<h1 class="page-title"><?php echo $p["title"]; ?></h1>
-				<span class="page-body">
-					<?php echo htmlspecialchars_decode($p["body"]); ?>
-				</span>
+		<div class="page-preview">
+			<a class="content" href="<?php echo BASE_URL .$p['url']; ?>" target="_blank">
+				<div>
+					<h1 class="page-title"><?php echo $p["title"]; ?></h1>
+					<span class="page-body" style="display: none;">
+						<?php echo htmlspecialchars_decode($p["body"]); ?>
+					</span>
+				</div>
+			</a>
+			<div class="options">
+				<a class="btn" href="<?php echo BASE_URL; ?>panel/edit/pages/<?php echo $p['id']; ?>"><i class="fas fa-pen"></i></a>
+				<a class="btn" href="<?php echo BASE_URL; ?>panel/delete/pages/<?php echo $p['id']; ?>"><i class="fas fa-trash"></i></a>
 			</div>
-		</a>
+		</div>
+		
 	<?php endforeach; ?>
+	<?php endif; ?>
 	</div>
 </div>
---
+++ mvc/cms/views/sidebar.php
@@ -0,0 +1,14 @@
+<h2>My Profile</h2>
+
+<h3>Name</h3>
+<p><?php echo $user["name"]; ?></p>
+
+<h3>E-mail</h3>
+<p><?php echo $user["email"]; ?></p>
+
+<style>
+	aside { padding: 1em; }
+	aside p { padding: 0.5em 0; }
+	aside h2 { margin: 0.25em 0 0.5em 0; text-align: center; }
+
+</style>
