97f2daccbb1f04f174193c17c28d8747d4b6cb04
ivanfilho21 <ivanfilho21@gmail.com>
2019-06-30 09:17:09 -0300

    Updates

--- object-oriented/cake_recipe/script.js
+++ object-oriented/cake_recipe/script.js
@@ -7,13 +7,22 @@ function addIngredient() {
 
 	if (ing.value != "" && parseInt(amount.value) > 0) {
 		list.innerHTML = old + "<li>" + amount.value + " " + ing.value + "</li>";
+
+		var a = document.createElement("input");
+		var i = document.createElement("input");
+		a.type = "hidden";
+		a.value = amount.value;
+		a.name = "amount";
+		i.type = "hidden";
+		i.value = ing.value;
+		i.name = "ingredient";
+
+		form.appendChild(a);
+		form.appendChild(i);
 	}
 
-	if (list.childNodes.length > 0) {
-		form.style.display = "block";
-	} else {
-		form.style.display = "none";
-	}
+	if (list.childNodes.length > 0) { form.style.display = "block"; }
+	else { form.style.display = "none"; }
 
 	focus(ing);
 }
--- object-oriented/cake_recipe/submit.php
+++ object-oriented/cake_recipe/submit.php
@@ -1,1 +1,5 @@
 <?php
+
+if ($_PHP["REQUEST_METHOD"] == "POST") {
+	
+}
