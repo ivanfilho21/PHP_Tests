f09252d81bbaace3bf702a14d04b2ec3c7d0bd1b
ivanfilho21 <ivanfilho21@gmail.com>
2019-07-01 09:57:28 -0300

    Updates

--- object-oriented/cake_recipe/submit.php
+++ object-oriented/cake_recipe/submit.php
@@ -1,5 +1,33 @@
 <?php
 
-if ($_PHP["REQUEST_METHOD"] == "POST") {
-	
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	$a["user"] = formatInput($_POST["user"]);
+	$a["ing"] = formatInput($_POST["ingredient"]);
+	$a["amount"] = formatInput($_POST["amount"]);
+
+	if (validation($a)) {
+		save($a);
+	}
+}
+header("Location: index.html");
+die;
+
+function formatInput($input)
+{
+	$input = trim($input);
+	$input = stripslashes($input);
+	$input = htmlspecialchars($input);
+
+	return $input;
+}
+
+function validation($a)
+{
+	$res = true;
+
+	if (empty($a["user"])) {
+		$res = false;
+	}
+
+	return $res;
 }
