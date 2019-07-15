5f4e61ac59d35d1f8207bb1bc4126c2b722105d8
ivanfilho21 <ivanfilho21@gmail.com>
2019-07-15 10:57:48 -0300

    Added ajax function to get colors from server

--- procedural/ballons/assets/js/script.js
+++ procedural/ballons/assets/js/script.js
@@ -29,6 +29,36 @@ function burst(ball) {
     document.body.removeChild(ball);
 }
 
-function start() {
+function createBalls() {
     setInterval(addBall, 1000);
+}
+
+function start() {
+    let url = "script.php";
+    let callback = function(response) {
+        colors = JSON.parse(response);
+        console.log(colors);
+        createBalls();
+    };
+    ajax(url, callback);
+}
+
+function ajax(url, callback, form="") {
+    var processResponseFunction = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            callback(this.responseText);
+        }
+    };
+
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = processResponseFunction;
+
+    if (form != "") {
+        let formData = new FormData(form);
+        xmlhttp.open("POST", url, true);
+        xmlhttp.send(formData);
+    } else {
+        xmlhttp.open("GET", url, true);
+        xmlhttp.send();
+    }
 }
