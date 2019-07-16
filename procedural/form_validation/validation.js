1aa8dd9b51719bbfde6eff401ffc0663f9df27bb
ivanfilho21 <ivanfilho21@gmail.com>
2019-07-16 12:44:59 -0300

    Updating

--- procedural/form_validation/validation.js
+++ procedural/form_validation/validation.js
@@ -1,18 +1,38 @@
+var nameRegex = /^[a-z0-9_-]{4,12}$/;
+var emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+var passRegex = /^[A-Za-z0-9]{6,}$/;
+
 function validation() {
     let form = document.getElementById("form");
-    let passRegex = /^[A-Za-z0-9]{6,}$/;
+    let name = form.username;
+    let email = form.email;
+    let pass = form.password;
+    let res = true;
 
-    if (! (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(form.email.value))) {
+    // Username
+    if (name.value.length < 4 || name.value.length > 12) {
+        res = false;
+    }
+    else if (! (nameRegex.test(name.value))) {
+        res = false;
+        console.log("Not a valid username");
+    }
+
+    // Email
+    if (! (emailRegex.test(email.value))) {
+        res = false;
         console.log("Not a valid Email");
     }
 
-    if (form.password.value.length < 6) {
-
-    } else if (form.password.value == form.username.value) {
-
-    } else if (! (passRegex.test(form.password.value))) {
+    // Password
+    if (pass.value.length < 6) {
+        res = false;
+    } else if (pass.value == name.value) {
+        res = false;
+    } else if (! (passRegex.test(pass.value))) {
+        res = false;
         console.log("Not a valid password");
     }
-    
-    return false;
+
+    return res;
 }
