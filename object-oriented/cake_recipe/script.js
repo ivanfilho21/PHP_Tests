368eb1779838b1c60306f8ec4914be3548f78aa5
ivanfilho21 <ivanfilho21@gmail.com>
2019-07-17 12:58:06 -0300

    Refactored the code and updated index

--- object-oriented/cake_recipe/script.js
+++ object-oriented/cake_recipe/script.js
@@ -4,32 +4,33 @@ function setRecipeName(name) {
 }
 
 function addIngredient() {
-	let amount = document.getElementById("amount");
-	let ing = document.getElementById("ingredient");
+	let form = document.getElementById("form");
+	let newForm = document.getElementById("new-form");
 	let list = document.getElementById("list");
 
-	let form = document.getElementById("form");
-	let txtArea = form.recipe;
-	txtArea.innerHTML = "";
+	let ing = form.ingredient;
+	let amount = form.amount;
+	let unity = form.unity;
+	let txtArea = newForm.recipe;
 
 	if (ing.value != "" && parseInt(amount.value) > 0) {
-		let v = amount.value + " " + ing.value;
+		let u = unity[unity.selectedIndex].value;
+		let aux = amount.value + " ";
+		let plural = (u.indexOf("y") != -1 ? u.substring(0, u.length-1) + "ies" : u + "s");
+		aux += parseInt(amount.value) > 1 ? plural : u;
+
+		let v = aux;
+		v += " of " + ing.value;
 		list.innerHTML += "<li>" + v + "</li>";
 		txtArea.innerHTML += v + "<br>";
 	}
 
-	if (list.childNodes.length > 0) { form.style.display = "block"; }
-	else { form.style.display = "none"; }
-
-	resetInputs();
-	return false;
-}
-
-function resetInputs() {
-	let amount = document.getElementById("amount");
-	let ing = document.getElementById("ingredient");
+	newForm.style.display = list.childNodes.length > 0 ? "block" : "none";
 
 	amount.value = 1;
+	unity.selectedIndex = 0;
 	ing.value = "";
 	ing.focus();
+
+	return false;
 }
