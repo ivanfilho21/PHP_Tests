b321129a7f183acaf149ad061da9fba44e6da90c
ivanfilho21 <ivanfilho21@gmail.com>
2019-07-20 17:18:34 -0300

    Updates

--- object-oriented/tasks_oo_2-0/config.php
+++ object-oriented/tasks_oo_2-0/config.php
@@ -1,10 +1,28 @@
 <?php
 
-define("DB_NAME", "tasks");
+define("ENVIRONMENT", "dev");
+define("DEBUGGING", true);
 define("DB_TYPE", "mysql");
-define("DB_HOST", "127.0.0.1");
-define("DB_USER", "root");
-define("DB_PASS", "");
+
+if (defined("ENVIRONMENT") && ENVIRONMENT == "dev") {
+    define("DB_NAME", "tasks");
+    define("DB_HOST", "127.0.0.1");
+    define("DB_USER", "root");
+    define("DB_PASS", "");
+} else {
+    define("DB_HOST", "sql213.epizy.com");
+    define("DB_NAME", "epiz_23646966_tasks_db");
+    define("DB_USER", "epiz_23646966");
+    define("DB_PASS", "ntFMNPatQ5EG");
+}
+
+if (defined("DEBUGGING") && DEBUGGING) {
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+} else {
+    ini_set('display_errors', 0);
+    error_reporting(E_STRICT);
+}
 
 # Database Connection
 $dsn = DB_TYPE . ":dbname=" . DB_NAME . ";host=" . DB_HOST;
@@ -14,7 +32,7 @@ $dbpass = DB_PASS;
 global $db;
 
 try {
-	$db = new PDO($dsn, $dbuser, $dbpass);
+    $db = new PDO($dsn, $dbuser, $dbpass);
     # echo "Connected to Database via PDO<br>"; die();
 } catch(PDOException $e) {
     echo "Warning: Failed connecting to database.<br><strong>Returned Error:</strong> " .$e->getMessage() . "<br>";
