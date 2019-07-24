fd46a22c4d25c3b22491a95a6414f9e23f51e0ef
ivanfilho21 <ivanfilho21@gmail.com>
2019-07-24 15:38:37 -0300

    Created routers.php

---
+++ mvc/forum/routers.php
@@ -0,0 +1,17 @@
+<?php
+
+global $routers;
+$routers = array();
+
+$routers["/"] = "/home/index";
+$routers["/index"] = "/home/index";
+$routers["/index/"] = "/home/index";
+$routers["/login"] = "/authentication/login";
+$routers["/login/"] = "/authentication/login";
+$routers["/sign-in"] = "/authentication/login";
+$routers["/sign-in/"] = "/authentication/login";
+$routers["/register"] = "/authentication/register";
+$routers["/register/"] = "/authentication/register";
+$routers["/sign-up"] = "/authentication/register";
+$routers["/sign-up/"] = "/authentication/register";
+// $routers["/login/{id}"] = "/authentication/login/:id";
