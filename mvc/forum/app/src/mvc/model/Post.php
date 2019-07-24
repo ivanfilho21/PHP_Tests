042ac239c90c2675e8def49a42fb53e194309113
ivanfilho21 <ivanfilho21@gmail.com>
2019-07-24 16:22:51 -0300

    Updates

--- mvc/forum/app/src/mvc/model/Post.php
+++ mvc/forum/app/src/mvc/model/Post.php
@@ -2,14 +2,13 @@
 
 class Post
 {
-    public function __construct($id = "", $author_id = "", $category_id = "", $title = "", $content = "", $publish_date = "", $update_date = "", $creation_date = "")
+    public function __construct($id = "", $author_id = "", $category_id = "", $title = "", $content = "", $update_date = "", $creation_date = "")
     {
         $this->id = $id;
         $this->author_id = $author_id;
         $this->category_id = $category_id;
         $this->title = $title;
         $this->content = $content;
-        $this->publish_date = $publish_date;
         $this->update_date = $update_date;
         $this->creation_date = $creation_date;
     }
@@ -22,8 +21,6 @@ class Post
     public function getCategoryId() { return $this->category_id; }
     public function setTitle($title) { $this->title = $title; }
     public function getTitle() { return $this->title; }
-    public function setPublishDate($publish_date) { $this->publish_date = $publish_date; }
-    public function getPublishDate() { return $this->publish_date; }
     public function setUpdateDate($update_date) { $this->update_date = $update_date; }
     public function getUpdateDate() { return $this->update_date; }
     public function setCreationDate($creation_date) { $this->creation_date = $creation_date; }
