3144ac76a12ac1713a5980ffef1f97b8022f6459
ivanfilho21 <ivanfilho21@gmail.com>
2019-07-27 10:23:23 -0300

    Created more validations and updates in script

--- mvc/forum/app/src/mvc/view/pages/authentication/js/sign-up-validation.js
+++ mvc/forum/app/src/mvc/view/pages/authentication/js/sign-up-validation.js
@@ -1,71 +1,114 @@
-var nameRegex = /[a-z0-9-]{6,}/;
+var usernameRegex = /[a-z0-9-]{6,}/;
 var emailRegex = /[a-zA-Z0-9-\.,]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-]{2,3}(\.[a-zA-Z0-9-]+)*/;
 var passRegex = /[\w]{6,}/;
-
-var inputName, inputEmail, inputPass1, inputPass2;
+var inputName, inputUsername, inputEmail, inputPass1, inputPass2, cb;
 
 function initInputs() {
-    inputName = document.getElementById("sign-up-form").username;
+    inputName = document.getElementById("sign-up-form").name;
+    inputUsername = document.getElementById("sign-up-form").username;
     inputEmail = document.getElementById("sign-up-form").email;
     inputPass1 = document.getElementById("sign-up-form").pass;
     inputPass2 = document.getElementById("sign-up-form").pass2;
+    cb = document.getElementById("user-terms");
+
+    console.log(document.getElementById("sign-up-form").childNodes);
 }
 
 function validation() {
-    let res = true;
-}
-
-function validateFieldValue(value, regex) {
-    return value && regex.test(value) ? true : false;
+    let res = validateName(inputName.value) &
+        validateUsername(inputUsername.value, false) &
+        validateEmail(inputEmail.value, false) &
+        validatePassword(inputPass1.value, false) &
+        validatePassword2(inputPass2.value, false) &
+        validateUserTerms(cb.checked, false) ? true : false;
+    return res;
 }
 
 function validateName(name) {
-    let error = document.getElementsByClassName("error-msg")[0];
-    let c = "";
-    if (name) {
-        if (! nameRegex.test(name)) {
-            c = "error";
-            error.innerHTML = "O nome de usuário deve ter no mínimo 6 caracteres.";
-        } else {
-            c = "success";
-            error.innerHTML = "";
-        }
-        inputName.setAttribute("class", c);
+    let res = true;
+
+    if (name.length <= 0) {
+        res = false;
     }
-    if (name.length == 0) {
-        inputName.removeAttribute("class");
-        error.innerHTML = "";
-    }
-    // console.log(emailRegex.test(name));
+
+    inputName.setAttribute("class", res ? "success" : "error");
+    return res;
 }
 
-function validateEmail(email) {
-    if (email && email.length >= 6) {
-        inputEmail.setAttribute("class", ! emailRegex.test(email) ? "error" : "success");
-        // console.log(emailRegex.test(email));
-    } else {
+function validateUsername(username, checkEmpty = true) {
+    let res = true;
+
+    if (! usernameRegex.test(username)) {
+        res = false;
+    }
+    inputUsername.setAttribute("class", res ? "success" : "error");
+
+    if (checkEmpty && username.length == 0) {
+        inputUsername.removeAttribute("class");
+        res = false;
+    }
+
+    return res;
+}
+
+function validateEmail(email, checkEmpty = true) {
+    let res = true;
+
+    if (! emailRegex.test(email)) {
+        res = false;
+    }
+    inputEmail.setAttribute("class", res ? "success" : "error");
+
+    if (checkEmpty && email.length == 0) {
         inputEmail.removeAttribute("class");
+        res = false;
     }
+
+    return res;
 }
 
-function validatePassword(pass) {
-    if (pass) {
-        inputPass1.setAttribute("class", ! passRegex.test(pass) ? "error" : "success");
-        // console.log(emailRegex.test(pass));
+function validatePassword(pass, checkEmpty = true) {
+    let res = true;
+
+    if (! passRegex.test(pass)) {
+        res = false;
     }
-    if (pass.length == 0) {
+    inputPass1.setAttribute("class", res ? "success" : "error");
+
+    if (checkEmpty && pass.length == 0) {
         inputPass1.removeAttribute("class");
+        res = false;
     }
+
+    return validatePassword2(inputPass2.value);
 }
 
-function validatePassword2(pass2) {
-    if (pass2) {
-        inputPass2.setAttribute("class", inputPass1.value != pass2 ? "error" : "success");
-        // console.log(emailRegex.test(pass));
+function validatePassword2(pass2, checkEmpty = true) {
+    let res = false;
+
+    if (inputPass1.value == pass2 && passRegex.test(pass2)) {
+        res = true;
     }
-    if (pass2.length == 0) {
+    inputPass2.setAttribute("class", res ? "success" : "error");
+
+    if (checkEmpty && pass2.length == 0) {
         inputPass2.removeAttribute("class");
+        res = false;
     }
+
+    return res;
+}
+
+function validateUserTerms(res) {
+    let cm = document.getElementsByClassName("cb-wrapper")[0].
+        getElementsByClassName("checkmark")[0];
+
+    if (! res)
+        cm.setAttribute("class", cm.getAttribute("class") + " " + "error");
+    else
+        cm.setAttribute("class", "checkmark");
+
+    return res; 
 }
 
 window.onload = initInputs;
