f8f78ac7deef89e1c8af935d479e764395183008
ivanfilho21 <ivanfilho21@gmail.com>
2019-07-28 10:45:40 -0300

    Cleaned code and updated validation via ajax

--- mvc/forum/app/src/mvc/view/pages/authentication/js/sign-up-validation.js
+++ mvc/forum/app/src/mvc/view/pages/authentication/js/sign-up-validation.js
@@ -1,35 +1,23 @@
-var usernameRegex = /[a-z0-9-]{6,}/;
-var emailRegex = /[a-zA-Z0-9-\.,]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-]{2,3}(\.[a-zA-Z0-9-]+)*/;
-var passRegex = /[\w]{6,}/;
-var inputName, inputUsername, inputEmail, inputPass1, inputPass2, cb, submit;
+var form, cb, submit;
+var inputs = [];
+var ajaxSent = false;
 var validationUrl = "scripts/auth-sign-up.php";
 
-var ajaxSent = false;
-var inputs = [];
-
 //
-// TODO: Refazer tudo via ajax :c
 // TODO: MOSTRAR MENSAGEM DE ERRO EM BALÃO OU AO PASSAR O MOUSE
 
 function initInputs() {
-    let form = document.getElementById("sign-up-form");
+    form = document.getElementById("sign-up-form");
     inputs["name"] = form.name;
     inputs["username"] = form.username;
     inputs["email"] = form.email;
     inputs["pass"] = form.pass;
     inputs["pass2"] = form.pass2;
-    // inputs["cb"] = document.getElementById("user-terms");
-
-    /*inputName = document.getElementById("sign-up-form").name;
-    inputUsername = document.getElementById("sign-up-form").username;
-    inputEmail = document.getElementById("sign-up-form").email;
-    inputPass1 = document.getElementById("sign-up-form").pass;
-    inputPass2 = document.getElementById("sign-up-form").pass2;*/
     cb = document.getElementById("user-terms");
     submit = document.getElementById("sign-up-form").save;
 }
 
-function validation(input) {
+function validation(all = false) {
     if (ajaxSent) return false;
 
     let callback = function(response) {
@@ -39,86 +27,44 @@ function validation(input) {
         ajaxSent = false;
         submit.removeAttribute("class");
 
-        if (input) {
-            input.setAttribute("class", response[input.name] == "1" ? "success" : "error");
-            if (response[input.name] == "0") input.removeAttribute("class");
-
-            // if (input.name == "pass" && response["pass2"] != "0") {
-                // alert("pass");
-                // inputs["pass2"].setAttribute("class", response["pass2"] == "1" ? "success" : "error");
-                // if (response[input.name] == "0") inputs["pass2"].removeAttribute("class");
-            // }
-            return;
-        }
-
         for (let key in response) {
             inputs[key].setAttribute("class", response[key] == "1" ? "success" : "error");
             if (response[key] == "0") inputs[key].removeAttribute("class");
         }
+
+        if (! all) return;
+
+        // Check validated
+        let valid = false;
+        for (let key in response) {
+            valid = response[key] == "1";
+            // console.log("v: ", valid);
+        }
+
+        // console.log("Validation ", valid);
+
+        if (valid) {
+            form.submit();
+        }
     }
 
     let url = validationUrl + "?";
-    if (input) {
-        url += input.name + "=" + input.value;
-        if (input.name == "pass2") url += "&pass=" + inputs["pass"].value;
-    } else {
-        url += "all=true&"
-        for (let key in inputs) {
-            let value = inputs[key].value;
-            url += key + "=" + value + "&";
-        }
-        url = url.substring(0, url.length - 1);
-    }
+    url += all ? "all=true&" : "";
+    
+    for (let key in inputs) {
+        if (inputs[key].value.length <= 0) continue;
 
-    ajax(url, callback);
+        let value = inputs[key].value;
+        url += key + "=" + value + "&";
+    }
+    url = url.substring(0, url.length - 1);
+
     ajaxSent = true;
     submit.setAttribute("class", "disabled");
+    ajax(url, callback);
 
     return false;
 }
-/*
-function validateName(name) {
-    let callback = function(response) {
-        input["name"].setAttribute("class", response == "1" ? "success" : "error");
-        if (response == "0") input["name"].removeAttribute("class");
-    };
-    ajax(validationUrl + "?name=" + name, callback);
-}
-
-function validateUsername(username, checkEmpty = true) {
-    let callback = function(response) {
-        inputUsername.setAttribute("class", response == "1" ? "success" : "error");
-        if (response == "0") inputUsername.removeAttribute("class");
-    };
-    ajax(validationUrl + "?username=" + username, callback);
-}
-
-function validateEmail(email, checkEmpty = true) {
-    let callback = function(response) {
-        console.log(response);
-        inputEmail.setAttribute("class", response == "1" ? "success" : "error");
-        if (response == "0") inputEmail.removeAttribute("class");
-    };
-    ajax(validationUrl + "?email=" + email, callback);
-}
-
-function validatePassword(pass, checkEmpty = true) {
-    let callback = function(response) {
-        console.log(response);
-        inputPass1.setAttribute("class", response == "1" ? "success" : "error");
-        if (response == "0") inputPass1.removeAttribute("class");
-    };
-    ajax(validationUrl + "?pass=" + pass + "&pass2=" + inputPass2.value, callback);
-}
-
-function validatePassword2(pass2, checkEmpty = true) {
-    let callback = function(response) {
-        console.log(response);
-        inputPass2.setAttribute("class", response == "1" ? "success" : "error");
-        if (response == "0") inputPass2.removeAttribute("class");
-    };
-    ajax(validationUrl + "?pass=" + inputPass1.value + "&pass2=" + pass2, callback);
-}*/
 
 function validateUserTerms(res, checkAlways = false) {
     let cm = document.getElementsByClassName("cb-wrapper")[0].
