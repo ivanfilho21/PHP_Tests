bd1306a87797011f10b8800bb515117b3ad5ea58
ivanfilho21 <ivanfilho21@gmail.com>
2019-08-05 06:12:48 -0300

    Updated URL constant

--- mvc/forum/config.php
+++ mvc/forum/config.php
@@ -15,10 +15,15 @@ if (defined("ENVIRONMENT") && ENVIRONMENT === "dev") {
     define("DB_HOST", "127.0.0.1");
     define("DB_USER", "root");
     define("DB_PASS", "");
-    define("URL", "http://localhost/dev/php-tests/mvc/forum/");
-    define("ASSETS", "http://localhost/dev/php-tests/mvc/forum/assets/");
-    define("REL_TEMPLATE", "http://localhost/dev/php-tests/mvc/forum/app/src/mvc/view/templates/");
-    define("REL_PAGE", "http://localhost/dev/php-tests/mvc/forum/app/src/mvc/view/pages/");
+    // define("URL", "http://localhost/dev/php-tests/mvc/forum/");
+    $array = explode("/", $_SERVER["PHP_SELF"]);
+    array_shift($array);
+    array_pop($array);
+    define("URL", $_SERVER["REQUEST_SCHEME"] ."://" .$_SERVER["SERVER_NAME"] ."/" .implode("/", $array) ."/");
+
+    define("ASSETS", URL ."assets/");
+    define("REL_TEMPLATE", URL ."forum/app/src/mvc/view/templates/");
+    define("REL_PAGE", URL ."app/src/mvc/view/pages/");
     define("SITE_NAME", "Forum - [DEV MODE]");
 } else {
     define("SITE_NAME", "Forum");
