9b53e77feddeeca687824b2e5c232b8395783a1a
ivanfilho21 <ivanfilho21@gmail.com>
2019-08-11 12:23:26 -0300

    Created Authentication class

---
+++ mvc/forum/app/src/packages/ivanfilho/authentication/src/Authentication.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace IvanFilho\Authentication;
+
+use \User;
+use \IvanFilho\Database\Table;
+
+class Authentication
+{
+    public function __construct(Table $userDAO)
+    {
+        $this->userDAO = $userDAO;
+    }
+
+    public function checkField($field, $value)
+    {
+        return $this->userDAO->get($field, $value) !== false;
+    }
+
+    public function getUser(array $fields)
+    {
+        return $this->userDAO->get($fields, null);
+    }
+
+    public function insertUser(User $user)
+    {
+        $this->userDAO->insert($user);
+    }
+
+    public function register(User $user)
+    {
+        echo "Registration";
+    }
+
+    public function login(User $user, bool $keepSession = false)
+    {
+        $this->setUserSession($user);
+
+        if ($keepSession) {
+            $this->setUserCookie($user);
+        }
+    }
+
+    public function securePassword($pass)
+    {
+        return md5($pass);
+    }
+
+    public function encode($str)
+    {
+        return urlencode(base64_encode($str));
+    }
+
+    public function decode($str)
+    {
+        return base64_decode(urldecode($str));
+    }
+
+    public function deleteUserSession()
+    {
+        unset($_SESSION["user-session"]);
+    }
+
+    public function deleteUserCookie()
+    {
+        setcookie("user-cookie", "", time()-3600, "/");
+    }
+
+    public function setUserSession(User $user)
+    {
+        $_SESSION["user-session"]["username"] = $user->getUsername();
+        $_SESSION["user-session"]["password"] = $user->getPassword();
+    }
+
+    public function setUserCookie(User $user)
+    {
+        # TODO: encrypt userInfo to put in a cookie
+        $username = $this->encode($user->getUsername());
+        $pass = $this->encode($user->getPassword());
+
+        setrawcookie("user-cookie", $username . md5(":") . $pass . "", time() + (86400 * 30), "/");
+    }
+
+    public function getLoggedUser()
+    {
+        $user = false;
+
+        if (isset($_SESSION["user-session"])) {
+            $un = $_SESSION["user-session"]["username"];
+            $pass = $_SESSION["user-session"]["password"];
+        } else if (isset($_COOKIE["user-cookie"])) {
+            $cookie = $_COOKIE["user-cookie"];
+            #echo $cookie; die();
+            $cookie = explode(md5(":"), $cookie);
+
+            $un = $this->decode($cookie[0]);
+            $pass = $this->decode($cookie[1]);
+        } else {
+            # No user is logged
+            return false;
+        }
+
+        # Now, user might not exist anymore in database.
+        # Check user in database
+
+        $user = $this->userDAO->get(array("username" => $un, "password" => $pass), null);
+
+        if (empty($user)) {
+            $this->deleteUserSession();
+            $this->deleteUserCookie();
+        }
+
+        return $user;
+    }
+}
