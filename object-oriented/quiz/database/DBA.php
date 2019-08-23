81b39bd9bd0ce1bdce65ad46165b56e4bcbd262e
ivanfilho21 <ivanfilho21@gmail.com>
2019-08-23 11:17:20 -0300

    Created DBA

---
+++ object-oriented/quiz/database/DBA.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Database;
+use \PDO;
+
+/**
+* Class: DBA
+* 
+* Holds all database table objects used in this project.
+*
+* @package      App
+* @subpackage   Database
+* @author       Ivan Filho <ivanfilho21@gmail.com>
+*
+* Created: July 20, 2019.
+* Last Modified: July 20, 2019.
+*/
+
+class DBA
+{
+    private static $instance = null;
+
+    public static function getInstance()
+    {
+        if (self::$instance === null) {
+            return new self();
+        }
+        return self::$instance;
+    }
+
+    public function getTable($name)
+    {
+        $table = false;
+        foreach ($this->tables as $t) {
+            if ($t->getName() === $name) {
+                $table = $t;
+                break;
+            }
+        }
+        return $table;
+    }
+
+    private function __construct()
+    {
+        $pdo = $this->getPDOConnection();
+        $this->tables[] = new Quiz($pdo);
+        // $this->tables[] = new Question($pdo);
+        // $this->tables[] = new Answer($pdo);
+
+        $this->createTables();
+    }
+
+    private function createTables()
+    {
+        foreach ($this->tables as $t) {
+            $t->create();
+        }
+    }
+
+    private function getPDOConnection()
+    {
+        $pdo = null;
+
+        try {
+            $pdo = new PDO("mysql:dbname=" .DB_NAME .";host=" .DB_HOST, DB_USER, DB_PASS);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e) {
+            die($e->getMessage());
+        }
+
+        return $pdo;
+    }
+}
