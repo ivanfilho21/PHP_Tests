53b83c63531e4b2e94954317de175a23bc13121e
ivanfilho21 <ivanfilho21@gmail.com>
2019-08-23 11:17:52 -0300

    Created QuizDAO

---
+++ object-oriented/quiz/database/Quiz.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Database;
+
+use \IvanFilho\Database\Table;
+use \IvanFilho\Database\Column;
+
+class Quiz extends Table
+{
+    public function __construct($pdo)
+    {
+        $c[] = new Column("id", INT, 11, false, "PRIMARY KEY", "AUTO_INCREMENT");
+        parent::__construct($pdo, "\Quiz", "quizes", $c);
+    }
+}
