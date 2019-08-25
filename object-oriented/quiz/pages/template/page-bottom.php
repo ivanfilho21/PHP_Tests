068b4889665d95022f3d91b6d4d0cbc753ee5793
ivanfilho21 <ivanfilho21@gmail.com>
2019-08-25 12:09:04 -0300

    Added creative tim's template

--- object-oriented/quiz/pages/template/page-bottom.php
+++ object-oriented/quiz/pages/template/page-bottom.php
@@ -1,2 +1,37 @@
+    </div>
+    <footer class="footer footer-black  footer-white ">
+      <div class="container-fluid">
+        <div class="row">
+          <nav class="footer-nav">
+            <ul>
+              <li>
+                <a href="https://www.creative-tim.com" target="_blank">Creative Tim</a>
+              </li>
+              <li>
+                <a href="http://blog.creative-tim.com/" target="_blank">Blog</a>
+              </li>
+              <li>
+                <a href="https://www.creative-tim.com/license" target="_blank">Licenses</a>
+              </li>
+            </ul>
+          </nav>
+          <div class="credits ml-auto">
+            <span class="copyright">
+              ©
+              <script>
+                document.write(new Date().getFullYear())
+              </script>, made with <i class="fa fa-heart heart"></i> by Creative Tim
+            </span>
+          </div>
+        </div>
+      </div>
+    </footer>
+    </div>
+  </div>
+  <!--   Core JS Files   -->
+  <script src="<?= $relPath ?>assets/js/core/jquery.min.js"></script>
+  <script src="<?= $relPath ?>assets/js/core/popper.min.js"></script>
+  <script src="<?= $relPath ?>assets/js/core/bootstrap.min.js"></script>
+  <script src="<?= $relPath ?>assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
 </body>
 </html>
--- object-oriented/quiz/pages/template/page-top.php
+++ object-oriented/quiz/pages/template/page-top.php
@@ -1,16 +1,78 @@
 <?php require $relPath ."config.php" ?>
+<?php
+
+$pages = array();
+$pages[] = array("title" => "Home", "url" => "index", "icon" => "shop");
+$pages[] = array("title" => "My Quizes", "url" => "pages/admin/my-quizes", "icon" => "single-copy-04");
+$pages[] = array("title" => "New Quiz", "url" => "pages/admin/new-quiz", "icon" => "simple-add");
+
+?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Quiz</title>
+  <meta charset="utf-8" />
+  <link rel="apple-touch-icon" sizes="76x76" href="<?= $relPath ?>assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="<?= $relPath ?>assets/img/favicon.png">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <title><?= $title ?></title>
+  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+  <!-- CSS Files -->
+  <link href="<?= $relPath ?>assets/css/bootstrap.min.css" rel="stylesheet" />
+  <link href="<?= $relPath ?>assets/css/paper-dashboard.css?v=2.0.0" rel="stylesheet" />
+  <!-- CSS Just for demo purpose, don't include it in your project -->
+  <link href="<?= $relPath ?>assets/demo/demo.css" rel="stylesheet" />
 </head>
-<body>
-    <header>
-        <nav>
-            <ul>
-                <li><a href="<?= $relPath ?>index.php">Home</a></li>
-                <li><a href="<?= $relPath ?>pages/admin/new-quiz.php">New Quiz</a></li>
-                <li><a href="<?= $relPath ?>pages/admin/my-quizes.php">My Quizes</a></li>
-            </ul>
-        </nav>
-    </header>
+
+<body class="">
+  <div class="wrapper ">
+    <div class="sidebar" data-color="white" data-active-color="danger">
+      <!--
+        Tip 1: You can change the color of the sidebar using: data-color="blue | green | orange | red | yellow"
+      -->
+      <div class="logo">
+        <a href="http://www.creative-tim.com" class="simple-text logo-mini">
+          <div class="logo-image-small">
+            <img src="<?= $relPath ?>assets/img/logo-small.png">
+          </div>
+        </a>
+        <a href="http://www.creative-tim.com" class="simple-text logo-normal">
+          Creative Tim
+          <!-- <div class="logo-image-big">
+            <img src="<?= $relPath ?>assets/img/logo-big.png">
+          </div> -->
+        </a>
+      </div>
+      <div class="sidebar-wrapper">
+        <ul class="nav" style="overflow: hidden;">
+        <?php foreach ($pages as $page): ?>
+          <li<?= ($title == $page["title"]) ? " class='active'" : "" ?>>
+            <a href="<?= $relPath .$page["url"] ?>.php">
+              <i class="nc-icon nc-<?= $page["icon"] ?>"></i>
+              <p><?= $page["title"] ?></p>
+            </a>
+          </li>
+        <?php endforeach ?>
+        </ul>
+      </div>
+    </div>
+    <div class="main-panel">
+      <!-- Navbar -->
+      <nav class="navbar navbar-expand-lg navbar-absolute fixed-top navbar-transparent">
+        <div class="container-fluid">
+          <div class="navbar-wrapper">
+            <div class="navbar-toggle">
+              <button type="button" class="navbar-toggler">
+                <span class="navbar-toggler-bar bar1"></span>
+                <span class="navbar-toggler-bar bar2"></span>
+                <span class="navbar-toggler-bar bar3"></span>
+              </button>
+            </div>
+            <a class="navbar-brand" href="<?= $relPath ?>index.php">Quizes</a>
+          </div>
+        </div>
+      </nav>
+      <!-- End Navbar -->
+<div class="content">
