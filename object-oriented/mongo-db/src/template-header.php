0d02d7f11f21378c635e9cc91eb6d30e90668a58
Ivan Filho <ivanfilho21@gmail.com>
2019-09-09 11:32:31 -0300

    Added bootstrap

---
+++ object-oriented/mongo-db/src/template-header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?= $title ?></title>
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+    <script defer src="../bootstrap/js/jquery.min.js"></script>
+    <script defer src="../bootstrap/js/bootstrap.bundle.min.js"></script>
+</head>
+<body class="bg-dark text-light">
+    <section class="container">
