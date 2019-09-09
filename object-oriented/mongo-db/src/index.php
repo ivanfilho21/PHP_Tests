6db0d14f5d055bfeb1f34dd0ac1e2d37e915455b
Ivan Filho <ivanfilho21@gmail.com>
2019-09-09 11:33:41 -0300

    Updates

--- object-oriented/mongo-db/src/index.php
+++ object-oriented/mongo-db/src/index.php
@@ -10,64 +10,41 @@ $megasena = $db->megasena;
 $docs = $megasena->find();
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="UTF-8">
-	<title>Teste com MongoDB</title>
-	<style>
-		body {
-			background: #333;
-			color: #eee;
-		}
+<?php $title = "Sorteios da MegaSena" ?>
+<?php require "template-header.php" ?>
 
-		table {
-			width: 100%;
-			border-collapse: collapse;
-		}
+<h1>Sorteios da MegaSena</h1>
+<h4>Quantidade: <?= $megasena->count() ?></h4>
 
-		thead {
-			background: #555;
-		}
+<a class="btn btn-primary" href="create.php">Novo Sorteio</a>
 
-		th {
-			padding: 0.5rem 0;
-		}
+<table class="table table-light table-striped">
+	<thead>
+		<tr>
+			<th>ID</th>
+			<th>#</th>
+			<th>Acumulou</th>
+			<th>Data</th>
+			<th>Options</th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php foreach ($docs as $doc): ?>
+		<?php if (empty($doc["Concurso"])) continue ?>
+		<tr>
+			<td><?= substr($doc["_id"], -9) ?></td>
+			<td><?= $doc["Concurso"] ?></td>
+			<td><?= mb_strtolower($doc["Acumulado"]) ?></td>
+			<td><?= $doc["Data Sorteio"] ?></td>
 
-		td {
-			padding: 0.5rem 0;
-			text-align: center;
-			background-color: #666;
-		}
-	</style>
-</head>
-<body>
-	<h1>Teste PHP e MongoDB</h1>
+			<td>
+				<a class="btn btn-info" href="view.php?<?= $doc["_id"] ?>">View</a>
+				<a class="btn btn-warning" href="edit.php?<?= $doc["_id"] ?>">Edit</a>
+				<a class="btn btn-danger" href="delete.php?<?= $doc["_id"] ?>">Delete</a>
+			</td>
+		</tr>
+		<?php endforeach ?>
+	</tbody>
+</table>
 
-	<h4>Database: <?= $megasena->getDatabaseName() ?></h4>
-	<h4>Collection: <?= $megasena->getCollectionName() ?></h4>
-	<h4>Nº of Docs: <?= $megasena->count() ?></h4>
-
-	<table>
-		<thead>
-			<tr>
-				<th>ID</th>
-				<th>#</th>
-				<th>Acumulou</th>
-				<th>Data</th>
-			</tr>
-		</thead>
-		<tbody>
-			<?php foreach ($docs as $doc): ?>
-			<?php if (empty($doc["Concurso"])) continue ?>
-			<tr>
-				<td><?= substr($doc["_id"], -9) ?></td>
-				<td><?= $doc["Concurso"] ?></td>
-				<td><?= $doc["Acumulado"] ?></td>
-				<td><?= $doc["Data Sorteio"] ?></td>
-			</tr>
-			<?php endforeach ?>
-		</tbody>
-	</table>
-</body>
-</html>
+<?php require "template-footer.php" ?>
