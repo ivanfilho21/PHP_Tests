df443c0ae74e4dfdbfbd4de884604158e78a59d8
Ivan Filho <ivanfilho21@gmail.com>
2019-09-11 12:18:47 -0300

    Add more fields to form

--- object-oriented/mongo-db/src/create.php
+++ object-oriented/mongo-db/src/create.php
@@ -30,17 +30,77 @@
     </div>
 
     <div class="form-group">
-        <label>Valor do Prêmio:</label>
-        <input class="form-control" type="text" name="prize" placeholder="R$ 999888,00" value="<?= $prize ?>">
-        <span class="text-danger"><?= (! isset($_SESSION["error"]["prize"])) ? "" : $_SESSION["error"]["prize"] ?></span>
-    </div>
-
-    <div class="form-group">
         <label>Acumulado?</label>
         <br>
         <label><input type="radio" name="ac" value="1" checked> Sim</label>
         <br>
         <label><input type="radio" name="ac" value="0"> Não</label>
+    </div>
+
+    <div class="form-group">
+        <label>Dezenas Sorteadas:</label>
+        <div class="row">
+            <?php for ($i = 0; $i < 6; $i++): ?>
+            <div class="col-sm">
+                <input class="form-control" type="number" name="dezena[]" min="1" max="60" value="<?= (! empty($dezenas[$i])) ? $dezenas[$i] : "1" ?>">
+                <br>
+            </div>
+            <?php endfor ?>
+        </div> 
+    </div>
+
+    <label class="h3">Sena</label>
+    <div class="row">
+        <div class="col-md-3">
+            <div class="form-group">
+                <label>Ganhadores:</label>
+                <input class="form-control" type="number" name="sena-qty" value="<?= (! empty($senaQty)) ? $senaQty : 0 ?>">
+            </div>
+
+        </div>
+        <div class="col-md">
+            <div class="form-group">
+                <label>Valor do Prêmio:</label>
+                <input class="form-control" type="text" name="prize-sena" placeholder="R$ 999888,00" value="<?= $prizeSena ?>">
+                <span class="text-danger"><?= (! isset($_SESSION["error"]["prize"])) ? "" : $_SESSION["error"]["prize"] ?></span>
+            </div>
+        </div>
+    </div>
+
+    <label class="h3">Quina</label>
+    <div class="row">
+        <div class="col-md-3">
+            <div class="form-group">
+                <label>Ganhadores:</label>
+                <input class="form-control" type="number" name="quina-qty" value="<?= (! empty($quinaQty)) ? $quinaQty : 0 ?>">
+            </div>
+
+        </div>
+        <div class="col-md">
+            <div class="form-group">
+                <label>Valor do Prêmio:</label>
+                <input class="form-control" type="text" name="prize-quina" placeholder="R$ 999888,00" value="<?= $prizeQuina ?>">
+                <span class="text-danger"><?= (! isset($_SESSION["error"]["prize"])) ? "" : $_SESSION["error"]["prize"] ?></span>
+            </div>
+        </div>
+    </div>
+
+    <label class="h3">Quadra</label>
+    <div class="row">
+        <div class="col-md-3">
+            <div class="form-group">
+                <label>Ganhadores:</label>
+                <input class="form-control" type="number" name="quadra-qty" value="<?= (! empty($quadraQty)) ? $quadraQty : 0 ?>">
+            </div>
+
+        </div>
+        <div class="col-md">
+            <div class="form-group">
+                <label>Valor do Prêmio:</label>
+                <input class="form-control" type="text" name="prize-quadra" placeholder="R$ 999888,00" value="<?= $prizeQuadra ?>">
+                <span class="text-danger"><?= (! isset($_SESSION["error"]["prize"])) ? "" : $_SESSION["error"]["prize"] ?></span>
+            </div>
+        </div>
     </div>
 
     <div class="row justify-content-center">
