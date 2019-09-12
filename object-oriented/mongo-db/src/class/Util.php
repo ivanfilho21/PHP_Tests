f10e52e02bb077b5d8fabdfb9cefc743ed1fa95c
Ivan Filho <ivanfilho21@gmail.com>
2019-09-12 12:20:29 -0300

    Create Util.php

---
+++ object-oriented/mongo-db/src/class/Util.php
@@ -0,0 +1,34 @@
+<?php
+
+class Util
+{
+    public static $ufList = array(
+        "AC" => ["name" => "Acre", "g" => "m"],
+        "AL" => ["name" => "Alagoas", "g" => "m"],
+        "AP" => ["name" => "Amapá", "g" => "m"],
+        "AM" => ["name" => "Amazonas", "g" => "m"],
+        "BA" => ["name" => "Bahia", "g" => "f"],
+        "CE" => ["name" => "Ceará", "g" => "m"],
+        "DF" => ["name" => "Distrito Federal", "g" => "m"],
+        "ES" => ["name" => "Espírito Santo", "g" => "m"],
+        "GO" => ["name" => "Goiás", "g" => "n"],
+        "MA" => ["name" => "Maranhão", "g" => "m"],
+        "MT" => ["name" => "Mato Grosso", "g" => "m"],
+        "MS" => ["name" => "Mato Grosso do Sul", "g" => "m"],
+        "MG" => ["name" => "Minas Gerais", "g" => "n"],
+        "PA" => ["name" => "Pará", "g" => "m"],
+        "PB" => ["name" => "Paraíba", "g" => "f"],
+        "PR" => ["name" => "Paraná", "g" => "m"],
+        "PE" => ["name" => "Pernambuco", "g" => "n"],
+        "PI" => ["name" => "Piauí", "g" => "m"],
+        "RJ" => ["name" => "Rio de Janeiro", "g" => "m"],
+        "RN" => ["name" => "Rio Grande do Norte", "g" => "m"],
+        "RS" => ["name" => "Rio Grande do Sul", "g" => "m"],
+        "RO" => ["name" => "Rondônia", "g" => "n"],
+        "RR" => ["name" => "Roraima", "g" => "n"],
+        "SC" => ["name" => "Santa Catarina", "g" => "n"],
+        "SP" => ["name" => "São Paulo", "g" => "n"],
+        "SE" => ["name" => "Sergipe", "g" => "n"],
+        "TO" => ["name" => "Tocantins", "g" => "n"],
+    );
+}
