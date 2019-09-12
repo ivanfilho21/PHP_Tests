a41c3644fa3b020b6bbb996164b32972da9368b4
Ivan Filho <ivanfilho21@gmail.com>
2019-09-12 12:24:03 -0300

    Use Util class

--- object-oriented/mongo-db/src/view.php
+++ object-oriented/mongo-db/src/view.php
@@ -1,5 +1,6 @@
 <?php $title = "Sorteio" ?>
 <?php require "template-header.php" ?>
+<?php require "class/Util.php" ?>
 <style>
     .ball {
         width: 3.5rem;
@@ -17,51 +18,8 @@
 $id = isset($_GET["id"]) ? $_GET["id"] : 0;
 if (empty($id)) header("Location: index.php");
 
-$ufList = array(
-    "AC" => ["name" => "Acre", "g" => "m"],
-    "AL" => ["name" => "Alagoas", "g" => "m"],
-    "AP" => ["name" => "Amapá", "g" => "m"],
-    "AM" => ["name" => "Amazonas", "g" => "m"],
-    "BA" => ["name" => "Bahia", "g" => "f"],
-    "CE" => ["name" => "Ceará", "g" => "m"],
-    "DF" => ["name" => "Distrito Federal", "g" => "m"],
-    "ES" => ["name" => "Espírito Santo", "g" => "m"],
-    "GO" => ["name" => "Goiás", "g" => "n"],
-    "MA" => ["name" => "Maranhão", "g" => "m"],
-    "MT" => ["name" => "Mato Grosso", "g" => "m"],
-    "MS" => ["name" => "Mato Grosso do Sul", "g" => "m"],
-    "MG" => ["name" => "Minas Gerais", "g" => "n"],
-    "PA" => ["name" => "Pará", "g" => "m"],
-    "PB" => ["name" => "Paraíba", "g" => "f"],
-    "PR" => ["name" => "Paraná", "g" => "m"],
-    "PE" => ["name" => "Pernambuco", "g" => "n"],
-    "PI" => ["name" => "Piauí", "g" => "m"],
-    "RJ" => ["name" => "Rio de Janeiro", "g" => "m"],
-    "RN" => ["name" => "Rio Grande do Norte", "g" => "m"],
-    "RS" => ["name" => "Rio Grande do Sul", "g" => "m"],
-    "RO" => ["name" => "Rondônia", "g" => "n"],
-    "RR" => ["name" => "Roraima", "g" => "n"],
-    "SC" => ["name" => "Santa Catarina", "g" => "n"],
-    "SP" => ["name" => "São Paulo", "g" => "n"],
-    "SE" => ["name" => "Sergipe", "g" => "n"],
-    "TO" => ["name" => "Tocantins", "g" => "n"],
-);
-
-$months = array(
-    "",
-    "Janeiro",
-    "Fevereiro",
-    "Março",
-    "Abril",
-    "Maio",
-    "Junho",
-    "Julho",
-    "Agosto",
-    "Setembro",
-    "Outubro",
-    "Novembro",
-    "Dezembro",
-);
+$ufList = Util::$ufList;
+$months = Util::$months;
 
 $docs = $conn->test->megasena->find(array("_id" => DB::getObjectId($id)));
 $doc = $docs->toArray()[0];
