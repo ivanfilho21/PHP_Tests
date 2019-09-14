e7d45f4d54ab19bf8a6b4661b27f972193dac5a2
Ivan Filho <ivanfilho21@gmail.com>
2019-09-14 09:26:30 -0300

    Add pagination

--- object-oriented/mongo-db/src/index.php
+++ object-oriented/mongo-db/src/index.php
@@ -1,11 +1,47 @@
 <?php $title = "Sorteios da MegaSena" ?>
 <?php require "template-header.php" ?>
 
-<?php $docs = DB::find("megasena", array(), "Concurso", 0, 13) ?>
+<?php
+$page = ! empty($_GET["p"]) ? $_GET["p"] : 1;
+$qtyPerPage = 25;
+$size = DB::count("megasena");
+
+$currentSkip = $page > 1 ? ($page - 1) * $qtyPerPage : 0;
+$pageCounter = ($qtyPerPage > 0) ? ceil($size / $qtyPerPage) : 1;
+
+$showLeft = 5; // Max of pages to the left
+$showRight = 3; // Max of pages to the right
+
+// echo "Page: " .$page .", skip: " .$currentSkip;
+?>
+<?php $docs = DB::find("megasena", array(), "Concurso", $currentSkip, $qtyPerPage) ?>
 
 <h1>Sorteios da MegaSena</h1>
 
 <a class="btn btn-primary" href="sorteio.php">Novo Sorteio</a>
+
+<style>
+	li {
+		white-space: nowrap;
+	}
+</style>
+<?php if ($pageCounter >= 1): ?>
+<ul class="pagination float-right">
+	<li class="page-item<?= ($page <= 1) ? " disabled" : "" ?>"><a href="?p=<?= ($page <= 1) ? $page : $page -1 ?>" class="page-link">❮ Anterior</a></li>
+
+	<?php for ($i = 0; $i < $pageCounter; $i++): ?>
+	<?php if ($i == $showLeft && ($i + 1) <= $pageCounter - $showRight): ?>
+	<li class="page-item"><a class="page-link">...</a></li>
+	<?php continue ?>
+	<?php endif ?>
+	<?php if ($i > $showLeft && $i < $pageCounter - $showRight): ?>
+	<?php continue ?>
+	<?php endif ?>
+	<li class="page-item<?= $page == ($i+1) ? " active" : "" ?>"><a class="page-link" href="?p=<?= $i + 1 ?>"><?= $i + 1 ?></a></li>
+	<?php endfor ?>
+	<li class="page-item<?= ($page >= $pageCounter) ? " disabled" : "" ?>"><a href="?p=<?= ($page >= $pageCounter) ? $page : $page +1 ?>" class="page-link">Próxima ❯</a></li>
+</ul>
+<?php endif ?>
 
 <table class="table table-light table-striped">
 	<thead>
