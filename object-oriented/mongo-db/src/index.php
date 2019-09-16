91640b6dcc4a12802eb5e3f567245ebd6ea98e72
Ivan Filho <ivanfilho21@gmail.com>
2019-09-16 17:02:04 -0300

    Move pagination code

--- object-oriented/mongo-db/src/index.php
+++ object-oriented/mongo-db/src/index.php
@@ -1,19 +1,6 @@
 <?php $title = "Sorteios da MegaSena" ?>
 <?php require "template-header.php" ?>
-
-<?php
-$page = ! empty($_GET["p"]) ? $_GET["p"] : 1;
-$qtyPerPage = 25;
-$size = DB::count("megasena");
-
-$currentSkip = $page > 1 ? ($page - 1) * $qtyPerPage : 0;
-$pageCounter = ($qtyPerPage > 0) ? ceil($size / $qtyPerPage) : 1;
-
-$showLeft = 5; // Max of pages to the left
-$showRight = 3; // Max of pages to the right
-
-// echo "Page: " .$page .", skip: " .$currentSkip;
-?>
+<?php require "scripts/pagination.php" ?>
 <?php $docs = DB::find("megasena", array(), "Concurso", $currentSkip, $qtyPerPage) ?>
 
 <h1>Sorteios da MegaSena</h1>
